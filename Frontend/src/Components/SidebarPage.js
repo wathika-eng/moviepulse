51e12d11e20c8308b182d0c43118210175ea716b
David Nanjila <nanjiladavid2@gmail.com>
2024-09-12 19:44:28 +0300

    Feat: Style the sidebar to be responsive
    Signed-off-by: David Nanjila <nanjiladavid2@gmail.com>

--- Frontend/src/Components/SidebarPage.js
+++ Frontend/src/Components/SidebarPage.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { FaHome, FaHeart, FaFire, FaCalendarAlt, FaUsers, FaCog, FaSignOutAlt } from 'react-icons/fa';
+import { FaHome, FaHeart, FaFire, FaCalendarAlt, FaUsers, FaCog, FaSignOutAlt, FaRegClock } from 'react-icons/fa';
 
 const SidebarPage = () => {
     return (
-        <div className="w-64 h-full bg-gray-800 text-white flex flex-col">
-            <ul className="flex-1">
+        <div className="w-64 h-screen bg-gray-800 text-white flex flex-col">
+            {/* Watch Icon Heading */}
+            <div className="flex items-center p-4 bg-gray-900 border-b border-gray-700">
+                <FaRegClock className="text-yellow-400 text-2xl mr-3" />
+                <span className="text-lg font-semibold">Watch List</span>
+            </div>
+
+            {/* Top Section: Home, Favorites, Trending, Coming Soon */}
+            <ul className="flex-1 mt-4">
                 <li className="flex items-center p-4 hover:bg-gray-700 cursor-pointer">
                     <FaHome className="text-blue-500 mr-3" />
                     <span>Home</span>
@@ -21,6 +28,10 @@ const SidebarPage = () => {
                     <FaCalendarAlt className="text-green-500 mr-3" />
                     <span>Coming Soon</span>
                 </li>
+            </ul>
+
+            {/* Middle Section: Community */}
+            <div className="flex flex-col mt-auto">
                 <li className="flex items-center p-4 hover:bg-gray-700 cursor-pointer">
                     <FaUsers className="text-purple-500 mr-3" />
                     <span>Community</span>
@@ -29,11 +40,15 @@ const SidebarPage = () => {
                     <FaCog className="text-gray-400 mr-3" />
                     <span>Social Settings</span>
                 </li>
+            </div>
+
+            {/* Bottom Section: Logout */}
+            <div className="flex flex-col mt-auto border-t border-gray-700">
                 <li className="flex items-center p-4 hover:bg-gray-700 cursor-pointer">
                     <FaSignOutAlt className="text-red-600 mr-3" />
                     <span>Log Out</span>
                 </li>
-            </ul>
+            </div>
         </div>
     );
 };
